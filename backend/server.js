733b0a08be58ff992dd5a6d4c58a95da898e0bde
SenadM02 <malkic.senad02@gmail.com>
2026-04-28 21:35:18 +0200

    change jwt to httpOnly

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 import express from "express";
 import cors from "cors";
 import authRoutes from './routes/auth.js';
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
 
